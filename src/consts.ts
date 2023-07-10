11a44652ec4df199a26886942281db52e08c7d46
cy <chen__yue.sina.cn>
2023-07-10 18:48:59 +0800

    提交基本文件

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'CY-个人博客';
+export const SITE_DESCRIPTION = 'Welcome to my blog!';
