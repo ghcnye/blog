f340deb23d446aa9f7875c09691e4c1937dd447b
cy <chen__yue.sina.cn>
2023-07-19 20:53:06 +0800

    111

--- src/pages/javascrit.js/header.js
+++
@@ -1,8 +0,0 @@
-// header.js 文件
-
-const userBox = document.querySelector('h2.username');
-userBox.addEventListener('click', (e) => {
-  const a = document.createElement('a');
-  a.href = '/'
-  a.click()
-});
